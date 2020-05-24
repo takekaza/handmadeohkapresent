88a28bb7883dfc4c9592fdabbb272a0b08538b68
Kazaki Takei <k.takei@solt-inc.com>
2020-05-24 15:03:28 +0900

    Update mailto.php

--- mailto.php
+++ mailto.php
@@ -8,9 +8,18 @@
 			mb_language("Japanese");
 			mb_internal_encoding("UTF-8");
 
-			$subject = $_POST['subject'];
-			$to = $_POST['to'];
-			$message = $_POST['message'];
+      if (!empty($_POST["subject"])) {
+            $subject = $_POST['subject'];
+        }
+      if (!empty($_POST["to"])) {
+            $to = $_POST['to'];
+        }
+      if (!empty($_POST["message"])) {
+            $message = $_POST['message'];
+        }
+			// $subject = $_POST['subject'];
+			// $to = $_POST['to'];
+			// $message = $_POST['message'];
       $header = "From: takekazaaaaaa@gmail.com";
 
 			if(mb_send_mail($to, $subject, $message, $header)){
