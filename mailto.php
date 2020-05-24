a0c91b25c3be28598e7c5ac1c63504c8a0380087
Kazaki Takei <k.takei@solt-inc.com>
2020-05-24 10:43:33 +0900

    Add files

---
+++ mailto.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<?php
+			mb_language("Japanese");
+			mb_internal_encoding("UTF-8");
+
+			$name = $_POST['name'];
+			$mail = $_POST['mail'];
+			$inquiry = $_POST['inquiry'];
+
+			if(mb_send_mail($name, $mail, $inquiry)){
+				echo "メールを送信しました";
+			} else {
+				echo "メールの送信に失敗しました";
+			}
+		?>
+	</body>
+</html>
